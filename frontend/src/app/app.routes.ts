519bd7f5f460fbc909ce2aaeeeda17a9ccaebd84
daadii <dadi1@speed.at>
2023-12-31 00:54:34 +0100

    added basic setup and removed useless start code of frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+/* Example Routes (Ex3 blatt-9)
+export const routes: Routes = [
+    {path: 'product/:id', component: ProductDetailComponent},
+    {path: 'product', component: ProductDetailComponent},
+    {path: 'products', component: ProductListComponent},
+    {path: 'about', component: AboutComponent},
+];
+ */
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
+import {provideRouter, withHashLocation} from "@angular/router";
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {routes} from "./app/app.routes";
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient(withFetch()),     // Neccessary for http client
+  ],
+})
   .catch((err) => console.error(err));
+
