e2f6adfa418b92e5c2fb87209a861ebd9efa1175
daadii <dadi1@speed.at>
2024-01-03 21:02:04 +0100

    added basic database creation script

---
+++ backend/database/createDB.sql
@@ -0,0 +1,32 @@
+-- Datenbank erstellbar im PSQL Tool mit: \i F:/Programmierung/Gitlab/Uni/web-dev/web-project/backend/database/createDB.sql
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+CREATE DATABASE webdev23project WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+ALTER DATABASE webdev23project OWNER TO postgres;
+
+\connect webdev23project
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+-- TODO create tables
