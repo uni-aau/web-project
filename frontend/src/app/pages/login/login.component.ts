f851d38eb0d4da4738ac9c9374c889191c78c79b
daadii <dadi1@speed.at>
2024-01-16 22:44:23 +0100

    added faster logging image loading

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core'
+import {Component, Input} from '@angular/core'
 import { Title, Meta } from '@angular/platform-browser'
 import {ComponentsModule} from "../../components/components.module";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ComponentsModule],
+  imports: [ComponentsModule, NgOptimizedImage],
   templateUrl: 'login.component.html',
   styleUrl: './login.component.css',
 })
 
 export class Login {
+  @Input()
+  image_src: string = '../../../assets/image.png'
+  @Input()
+  image_alt: string = 'image'
   constructor(private title: Title, private meta: Meta) {
     this.title.setTitle('Login - Diligent Jam Packed Wallaby')
     this.meta.addTags([
