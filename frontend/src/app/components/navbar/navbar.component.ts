154c609fe699baf488023bae179cfdcd1aea009a
daadii <dadi1@speed.at>
2024-01-16 21:06:10 +0100

    updated links in navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -9,17 +9,11 @@ export class Navbar {
   @Input()
   rootClassName: string = ''
   @Input()
-  link_navbar_link2: string = 'https://example.com'
-  @Input()
   text_navbar_link3: string = 'Pricing'
-  @Input()
-  link_navbar_link1: string = 'https://example.com'
   @Input()
   text_navbar_link31: string = 'About'
   @Input()
   button_navbar_basket_text: string = 'Warenkorb (%s)'
-  @Input()
-  link_navbar_link3: string = 'https://example.com'
   @Input()
   button_navbar_login_text: string = 'Login'
   @Input()
