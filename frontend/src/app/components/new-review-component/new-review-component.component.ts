397e84f0108cad433e106a4c5867fe043f22577c
daadii <dadi1@speed.at>
2024-01-18 21:21:07 +0100

    forgot to add pages

---
+++ frontend/src/app/components/new-review-component/new-review-component.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+  selector: 'new-review-component',
+  templateUrl: 'new-review-component.component.html',
+  styleUrls: ['new-review-component.component.css'],
+})
+export class NewReviewComponent {
+  @Input()
+  review_heading: string = 'Create Review'
+  @Input()
+  review_rating_title: string = 'Rating'
+  @Input()
+  review_delete_review: string = '(Delete)'
+  @Input()
+  review_input_title: string = 'Enter Title'
+  @Input()
+  review_input_title_placeholder: string =
+    'What is the most important information?'
+  @Input()
+  review_input_description: string = 'Enter a description for your rating'
+  @Input()
+  review_input_description_placeholder: string =
+    'What did you like? What did you dislike?'
+  @Input()
+  button_send: string = 'Send'
+  @Input()
+  rootClassName: string = ''
+  constructor() {}
+}
