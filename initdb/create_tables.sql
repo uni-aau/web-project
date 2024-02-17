2d98b78ca133053e38cbe87b9c3e5eb215d6eedf
daadii <dadi1@speed.at>
2024-02-17 16:05:42 +0100

    updated parkingspot & bike table

--- initdb/create_tables.sql
+++ initdb/create_tables.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS BikeModelReview, StationReview, ParkingSpot, Transaction, Ticket, Bike, BikeModel, BikeCategory, Station, Wallet, "User" CASCADE;
+DROP TYPE IF EXISTS bike_status, category_status, model_status;
+
+CREATE TYPE bike_status AS ENUM ('Available', 'Booked', 'Rented', 'Maintenance');
 
 CREATE TABLE Wallet
 (
@@ -8,15 +11,15 @@ CREATE TABLE Wallet
 
 CREATE TABLE "User"
 (
-    user_id       SERIAL PRIMARY KEY,
-    username      VARCHAR UNIQUE NOT NULL,
-    firstname     VARCHAR        NOT NULL,
-    lastname      VARCHAR        NOT NULL,
-    email         VARCHAR UNIQUE NOT NULL,
-    is_admin      BOOLEAN        NOT NULL,
-    password_hash VARCHAR        NOT NULL,
-    wallet_id     INT UNIQUE REFERENCES Wallet (wallet_id),
-    profile_picture_location VARCHAR
+    user_id                  SERIAL PRIMARY KEY,
+    username                 VARCHAR UNIQUE NOT NULL,
+    firstname                VARCHAR        NOT NULL,
+    lastname                 VARCHAR        NOT NULL,
+    email                    VARCHAR UNIQUE NOT NULL,
+    is_admin                 BOOLEAN        NOT NULL,
+    password_hash            VARCHAR        NOT NULL,
+    wallet_id                INT UNIQUE REFERENCES Wallet (wallet_id),
+    profile_picture_location VARCHAR DEFAULT '/assets/no-image.svg'
 );
 
 CREATE TABLE Station
@@ -46,20 +49,19 @@ CREATE TABLE BikeModel
     name        VARCHAR NOT NULL,
     price       FLOAT,
     status      VARCHAR,
-    category_id INT REFERENCES BikeCategory (category_id),
+    category_id INT REFERENCES BikeCategory (category_id)
 );
 
--- TODO enum + mehrere kategorien, also extra table
 CREATE TABLE Bike
 (
-    bike_id      SERIAL PRIMARY KEY,
-    station_id   INT REFERENCES Station (station_id),
-    model_id     INT REFERENCES BikeModel (model_id),
-    is_available BOOLEAN DEFAULT TRUE,
-    status VARCHAR,
-    size INT NOT NULL,
-    price FLOAT,
-    bike_image_location VARCHAR
+    bike_id             SERIAL PRIMARY KEY,
+    station_id          INT REFERENCES Station (station_id),
+    model_id            INT REFERENCES BikeModel (model_id),
+    is_available        BOOLEAN DEFAULT TRUE,
+    status              bike_status NOT NULL,
+    size                INT         NOT NULL,
+    price               FLOAT,
+    bike_image_location VARCHAR DEFAULT '/assets/no-image.svg'
 );
 
 -- TODO
@@ -83,15 +85,22 @@ CREATE TABLE Transaction
     timestamp        TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- // TODO one or mult categories -> extra relation
 CREATE TABLE ParkingSpot
 (
     spot_id     SERIAL PRIMARY KEY,
     station_id  INT REFERENCES Station (station_id),
     spot_number INT,
-    category_id INT REFERENCES BikeCategory (category_id),
     CONSTRAINT unique_spot UNIQUE (station_id, spot_number)
 );
+
+CREATE TABLE ParkingSpotCategory
+(
+    spot_id     INT,
+    category_id INT,
+    PRIMARY KEY (spot_id, category_id),
+    FOREIGN KEY (spot_id) REFERENCES ParkingSpot (spot_id),
+    FOREIGN KEY (category_id) REFERENCES BikeCategory (category_id)
+)
 
 CREATE TABLE StationReview
 (
