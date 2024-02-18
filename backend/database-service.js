863680347e371a115ef074ba4bfff36eb8364f9b
daadii <dadi1@speed.at>
2024-02-18 23:29:03 +0100

    added database service

---
+++ backend/database-service.js
@@ -0,0 +1,35 @@
+const pool = require("./pool");
+
+class DatabaseService {
+    static executeSelectionQuery(query) {
+        console.log("Executing: " + query.text + " with values: " + query.values);
+
+        return new Promise((resolve, reject) => {
+            pool.query(query)
+                .then(results => {
+                    if (results.rows.length <= 0) reject(new Error("Now rows found to select"));
+                    else resolve(results.rows);
+                })
+                .catch(error => {
+                    reject(new Error("Error fetching product: " + error.message));
+                });
+        });
+    }
+
+    static executeUpdateQuery(query) {
+        console.log("Executing: " + query.text + " with values: " + query.values);
+
+        return new Promise((resolve, reject) => {
+            pool.query(query)
+                .then(results => {
+                    if (results.rowCount <= 0) reject(new Error("No data changed"));
+                    else resolve(results.rowCount);
+                })
+                .catch(error => {
+                    reject(error.message)
+                })
+        })
+    }
+}
+
+module.exports = DatabaseService;
