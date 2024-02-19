94198ccf790b4f3377f94f4b0868fa57704371e7
daadii <dadi1@speed.at>
2024-02-19 19:32:57 +0100

    added reviews service

---
+++ frontend/src/app/services/reviews.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+const baseUrl = "http://localhost:3000/api/stationReviews";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  fetchStationReviews(stationId: number): Observable<any> {
+    return this.http.get(`${baseUrl}/station/${stationId}`);
+  }
+}
