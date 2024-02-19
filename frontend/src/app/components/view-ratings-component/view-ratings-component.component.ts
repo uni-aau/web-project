ce9e0cbc87313b70f3424ca3a1aac1b811c9477a
daadii <dadi1@speed.at>
2024-02-19 19:33:21 +0100

    added basic implementation of review list

--- frontend/src/app/components/view-ratings-component/view-ratings-component.component.ts
+++ frontend/src/app/components/view-ratings-component/view-ratings-component.component.ts
@@ -1,14 +1,34 @@
-import { Component, Input } from '@angular/core'
+import {Component, Input} from '@angular/core'
+import {ReviewsService} from "../../services/reviews.service";
+
 
 @Component({
-  selector: 'view-ratings-component',
-  templateUrl: 'view-ratings-component.component.html',
-  styleUrls: ['view-ratings-component.component.css'],
+    selector: 'view-ratings-component',
+    templateUrl: 'view-ratings-component.component.html',
+    styleUrls: ['view-ratings-component.component.css'],
 })
 export class ViewRatingsComponent {
-  @Input()
-  rootClassName: string = ''
-  @Input()
-  viewRatingsTitle: string = 'Reviews'
-  constructor() {}
+    @Input()
+    rootClassName: string = ''
+    @Input()
+    viewRatingsTitle: string = 'Reviews'
+
+    reviews: any[] = [];
+
+    constructor(private reviewsService: ReviewsService) {
+        this.reviews = [];
+        this.fetchStationReviews();
+    }
+
+    fetchStationReviews() {
+        this.reviewsService.fetchStationReviews(1).subscribe({
+            next: (res) => {
+                this.reviews = res;
+                console.log(this.reviews);
+            },
+            error: (err) => {
+                console.log(err.error);
+            }
+        })
+    }
 }
