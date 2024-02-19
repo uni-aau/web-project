549ff12c9fe8fc0d1834611023e2890e0420a5ca
daadii <dadi1@speed.at>
2024-02-19 13:53:00 +0100

    reworked bike station backend

--- backend/routes/station.js
+++ backend/routes/station.js
@@ -1,63 +1,65 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../pool');
+const DatabaseService = require("../database-service");
 
 router.get('/', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM station');
-        res.json(result.rows);
-    } catch (err) {
-        console.error('Fehler beim Abrufen der Stations:', err.stack);
-        res.status(500).send('Serverfehler');
-    }
+    DatabaseService.executeSelectionQuery({text: 'SELECT * FROM Station', values: []})
+        .then(results => res.status(200).json(results))
+        .catch(e => {
+            if (e.message === "Nothing found") res.status(404).json({error: e})
+            else res.status(500).json({error: "Error while fetching stations: " + e})
+        });
 });
 
-router.post('/', async (req, res) => {
-    const { name, location, longitude, latitude } = req.body;
-    try {
-        const result = await pool.query(
-            'INSERT INTO station (name, location, longitude, latitude) VALUES ($1, $2, $3, $4) RETURNING *',
-            [name, location, longitude, latitude]
-        );
-        res.status(201).json(result.rows[0]);
-    } catch (err) {
-        console.error('Fehler beim Hinzufügen der Station:', err.stack);
-        res.status(500).send('Serverfehler');
-    }
+router.get('/station/:stationId', function (req, res) {
+    const {stationId} = req.params;
+
+    DatabaseService.executeSelectionQuery({text: 'SELECT * FROM Station WHERE station_id = $1', values: [stationId]})
+        .then(results => res.status(200).json(results))
+        .catch(e => {
+            if (e.message === "Nothing found") res.status(404).json({error: e})
+            else res.status(500).json({error: "Error while fetching bike: " + e})
+        });
 });
 
-router.put('/:stationId', async (req, res) => {
-    const { stationId } = req.params;
-    const { name, location, longitude, latitude } = req.body;
-    try {
-        const result = await pool.query(
-            'UPDATE station SET name = $1, location = $2, longitude = $3, latitude = $4 WHERE station_id = $5 RETURNING *',
-            [name, location, longitude, latitude, stationId]
-        );
-        if (result.rows.length > 0) {
-            res.json(result.rows[0]);
-        } else {
-            res.status(404).send('Station nicht gefunden');
-        }
-    } catch (err) {
-        console.error('Fehler beim Aktualisieren der Station:', err.stack);
-        res.status(500).send('Serverfehler');
+router.post('/station', async (req, res) => {
+    const {stationName, description, stationAddress, longitude, latitude, stationImageLocation} = req.body;
+
+    if (!stationName || !stationAddress || !longitude || !latitude) return res.status(500).json({error: "Not all required data inserted"});
+
+    let query = {
+        text: 'INSERT INTO station (station_name, description, station_address, longitude, latitude, station_image_location) VALUES($1, $2, $3, $4, $5, $6)',
+        values: [stationName, description, stationAddress, longitude, latitude, stationImageLocation]
     }
+
+    DatabaseService.executeInsertionQuery(query)
+        .then(result => res.status(200).json({success: true, rowsChanged: result}))
+        .catch(e => res.status(500).json({error: "Error while adding bike station: " + e}))
 });
 
-router.delete('/:stationId', async (req, res) => {
-    const { stationId } = req.params;
-    try {
-        const result = await pool.query('DELETE FROM station WHERE station_id = $1 RETURNING *', [stationId]);
-        if (result.rows.length > 0) {
-            res.send('Station gelöscht');
-        } else {
-            res.status(404).send('Station nicht gefunden');
-        }
-    } catch (err) {
-        console.error('Fehler beim Löschen der Station:', err.stack);
-        res.status(500).send('Serverfehler');
+router.put('/station/:stationId', async (req, res) => {
+    const {stationId} = req.params;
+    const {stationName, description, stationAddress, longitude, latitude, stationImageLocation} = req.body;
+
+    if (!stationName || !stationAddress || !longitude || !latitude) return res.status(500).json({error: "Not all required data inserted"});
+
+    let query = {
+        text: 'UPDATE station SET station_name = $1, description = $2, station_address = $3, longitude = $4, latitude = $5, station_image_location = $6 WHERE station_id = $7',
+        values: [stationName, description, stationAddress, longitude, latitude, stationImageLocation, stationId]
     }
+
+    DatabaseService.executeUpdateQuery(query)
+        .then(result => res.status(200).json({success: true, rowsChanged: result}))
+        .catch(e => res.status(500).json({error: "Error while updating bike station: " + e}))
+});
+
+router.delete('/station/:stationId', async (req, res) => {
+    const {stationId} = req.params;
+
+    DatabaseService.executeDeleteQuery({text: 'DELETE FROM station WHERE station_id = $1', values: [stationId]})
+        .then(result => res.status(200).json({success: true, rowsChanged: result}))
+        .catch(e => res.status(500).json({error: "Error while deleting bike station: " + e}))
 });
 
 module.exports = router;
