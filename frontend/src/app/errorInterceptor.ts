db9425af43f5717f4a391ca23f203a5813bb141c
jublebi <julianlechner31@gmail.com>
2024-02-20 20:23:31 +0100

    Implemented error Intercepor which redirects if permission denied (403) to the login page. This happens due to missing or expired Auth header.

---
+++ frontend/src/app/errorInterceptor.ts
@@ -0,0 +1,24 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: any) => {
+        if (error instanceof HttpErrorResponse && error.status === 403) {
+          // Bei Statuscode 403 einen Redirect durchführen
+          this.router.navigate(['/login']);
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
+
+
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,18 +2,31 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {appConfig} from './app/app.config';
 import {AppComponent} from './app/app.component';
 import {provideRouter, withHashLocation} from "@angular/router";
-import {provideHttpClient, withFetch, withInterceptors} from "@angular/common/http";
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+  withInterceptorsFromDi
+} from "@angular/common/http";
 import {routes} from "./app/app.routes";
 import {spinnerInterceptor} from "./app/spinner.interceptor";
 import {authInterceptor} from "./app/auth.interceptor";
+import {ErrorInterceptor} from "./app/errorInterceptor";
+
+
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes, withHashLocation()),
+
     provideHttpClient(
+      withInterceptorsFromDi(),
       withFetch(),
-      withInterceptors([spinnerInterceptor, authInterceptor])
-    )
+      withInterceptors([spinnerInterceptor, authInterceptor]),
+
+    ),
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
 })
   .catch((err) => console.error(err));
