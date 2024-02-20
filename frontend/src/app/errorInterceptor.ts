ccad7c1b9bc601930e25399b04a980af75a2c234
jublebi <julianlechner31@gmail.com>
2024-02-20 20:27:06 +0100

    Added logout if redirect to login page happens to delete local storage and cookies

--- frontend/src/app/errorInterceptor.ts
+++ frontend/src/app/errorInterceptor.ts
@@ -3,16 +3,17 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import {Injectable} from "@angular/core";
+import {AuthService} from "./services/auth.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService:AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error: any) => {
         if (error instanceof HttpErrorResponse && error.status === 403) {
-          // Bei Statuscode 403 einen Redirect durchführen
+          this.authService.logout();
           this.router.navigate(['/login']);
         }
         return throwError(error);
