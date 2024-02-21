737ea6da860be26e4efd9d74ab9cf146f0f6c9c4
daadii <dadi1@speed.at>
2024-02-21 21:15:54 +0100

    implemented delete ratings button

--- frontend/src/app/components/admin-bike-station-entry-component/admin-bike-station-entry-component.component.ts
+++ frontend/src/app/components/admin-bike-station-entry-component/admin-bike-station-entry-component.component.ts
@@ -2,99 +2,139 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core'
 import {Router} from "@angular/router";
 import {BikeStation} from "../../types/bikeStation.type";
 import {ReviewsService} from "../../services/reviews.service";
+import {PopupService} from "../../services/popup.service";
+import {BikeStationService} from "../../services/bikestation.service";
 
 @Component({
-  selector: 'admin-bike-station-entry-component',
-  templateUrl: 'admin-bike-station-entry-component.component.html',
-  styleUrls: ['admin-bike-station-entry-component.component.css'],
+    selector: 'admin-bike-station-entry-component',
+    templateUrl: 'admin-bike-station-entry-component.component.html',
+    styleUrls: ['admin-bike-station-entry-component.component.css'],
 })
 export class AdminBikeStationEntryComponent implements OnInit {
-  @Output() update = new EventEmitter<string>();
-  @Input()
-  bikeStation: BikeStation = {
-    description: "string",
-    latitude: 0,
-    longitude: 0,
-    station_address: "string",
-    station_image_location: "string",
-    station_name: "string",
-    station_id: 0
-  };
-  @Input()
-  adminBikeStationEntryButtonUpdate: string = 'Update'
-  @Input()
-  adminBikeStationEntryImageSrc: string = '/assets/no-image.svg'
-  @Input()
-  adminBikeStationEntryButtonDeleteRatings: string = 'Delete Ratings'
-  @Input()
-  adminBikeStationEntryDescription: string = '{0}'
-  @Input()
-  adminBikeStationEntryButtonDelete: string = 'Delete'
-  @Input()
-  adminBikeStationEntryImageAlt: string = 'image'
-  @Input()
-  adminBikeStationEntryStationName: string = '{0}'
-  @Input()
-  adminBikeStationEntryStarRatingNumber: string = '({0})'
-  @Input()
-  adminBikeStationEntryButtonManageRatings: string = 'Manage Ratings'
-  @Input()
-  adminBikeStationEntryLocation: string = '{0}'
-  @Input()
-  rootClassName: string = ''
+    @Output() update = new EventEmitter<string>();
+    @Input()
+    bikeStation: BikeStation = {
+        description: "string",
+        latitude: 0,
+        longitude: 0,
+        station_address: "string",
+        station_image_location: "string",
+        station_name: "string",
+        station_id: 0
+    };
+    @Input()
+    adminBikeStationEntryButtonUpdate: string = 'Update'
+    @Input()
+    adminBikeStationEntryImageSrc: string = '/assets/no-image.svg'
+    @Input()
+    adminBikeStationEntryButtonDeleteRatings: string = 'Delete Ratings'
+    @Input()
+    adminBikeStationEntryDescription: string = '{0}'
+    @Input()
+    adminBikeStationEntryButtonDelete: string = 'Delete'
+    @Input()
+    adminBikeStationEntryImageAlt: string = 'image'
+    @Input()
+    adminBikeStationEntryStationName: string = '{0}'
+    @Input()
+    adminBikeStationEntryStarRatingNumber: string = '({0})'
+    @Input()
+    adminBikeStationEntryButtonManageRatings: string = 'Manage Ratings'
+    @Input()
+    adminBikeStationEntryLocation: string = '{0}'
+    @Input()
+    rootClassName: string = ''
 
-  noRatingsText = "(No Ratings)";
-  ratingsErrorText = "(Error)";
+    @Output() onStationDelete: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onStationUpdate: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onStationReviewDelete: EventEmitter<any> = new EventEmitter<any>();
 
-  ratingNumber = Array(1).fill(0).map((x, i) => i);
-  ratingNumberReversed = Array(5).fill(0).map((x, i) => i);
-  maxRating = 5;
+    noRatingsText = "(No Ratings)";
+    ratingsErrorText = "(Error)";
+    popupDeleteStationConfirmationText = "Do you really want to delete the bike station?"
+    popupDeleteReviewsConfirmationText = "Do you really want to delete all ratings of this station?"
+    ratingsAmount = 0;
 
-  constructor(private router: Router, private reviewService: ReviewsService) {
-  }
+    ratingNumber = Array(1).fill(0).map((x, i) => i);
+    ratingNumberReversed = Array(5).fill(0).map((x, i) => i);
+    maxRating = 5;
 
-  ngOnInit() {
-    this.fetchData();
-  }
+    constructor(private router: Router, private reviewService: ReviewsService, private popupService: PopupService, private stationService: BikeStationService) {
+    }
 
-  fetchData() {
-    this.reviewService.fetchStationReviews(this.bikeStation.station_id).subscribe(
-      {
-        next: (data) => {
-          let sum = 0;
-          data.forEach((rate: any) => {
-            sum += rate.rating
-          })
-          sum = sum / data.length;
-          this.adminBikeStationEntryStarRatingNumber = "(" + String(sum) + ")";
-          const rating = data[0].rating;
-          this.ratingNumber = Array(rating).fill(0).map((x, i) => i);
-          this.ratingNumberReversed = Array(this.maxRating - rating).fill(0).map((x, i) => i);
-        },
-        error: (err) => {
-          if (err.status === 404) this.adminBikeStationEntryStarRatingNumber = this.noRatingsText;
-          else this.adminBikeStationEntryStarRatingNumber = this.ratingsErrorText;
+    ngOnInit() {
+        this.fetchData();
+    }
 
-          this.ratingNumber = Array(0).fill(0).map((x, i) => i);
-          this.ratingNumberReversed = Array(5).fill(0).map((x, i) => i);
-        }
-      })
-  }
+    fetchData() {
+        this.reviewService.fetchStationReviews(this.bikeStation.station_id).subscribe(
+            {
+                next: (data) => {
+                    let sum = 0;
+                    data.forEach((rate: any) => {
+                        sum += rate.rating
+                        this.ratingsAmount++;
+                    })
+                    sum = sum / data.length;
+                    this.adminBikeStationEntryStarRatingNumber = "(" + String(sum) + ")";
+                    const rating = data[0].rating;
+                    this.ratingNumber = Array(rating).fill(0).map((x, i) => i);
+                    this.ratingNumberReversed = Array(this.maxRating - rating).fill(0).map((x, i) => i);
+                },
+                error: (err) => {
+                    if (err.status === 404) this.adminBikeStationEntryStarRatingNumber = this.noRatingsText;
+                    else this.adminBikeStationEntryStarRatingNumber = this.ratingsErrorText;
+
+                    this.ratingNumber = Array(0).fill(0).map((x, i) => i);
+                    this.ratingNumberReversed = Array(5).fill(0).map((x, i) => i);
+                }
+            })
+    }
+
+    onDeleteRatings() {
+        if (this.ratingsAmount > 0) {
+            this.popupService.openPopup(this.popupDeleteReviewsConfirmationText).subscribe(result => {
+                if (result) {
+                    this.executeRatingDeletionQuery()
+                }
+            })
+        } else alert("No ratings available to delete!");
+    }
+
+    executeRatingDeletionQuery() {
+        this.reviewService.deleteAllStationReviews(this.bikeStation.station_id).subscribe({
+            next: (val) => this.onStationReviewDelete.emit(),
+            error: (err) => console.log("Error while deleting station ratings: ", err)
+        })
+    }
+
+    onDeleteStation() {
+        // TODO IF
+        this.popupService.openPopup(this.popupDeleteStationConfirmationText).subscribe(result => {
+            if (result) {
+                this.executeStationDeletionQuery();
+            }
+        })
+    }
+
+    executeStationDeletionQuery() {
+        // TODO
+    }
 
 
-  onUpdate() {
-    this.router.navigate(["/admin-manage-bike-station"], {
-      state: {
-        bikeStation: this.bikeStation
-      }
-    })
-  }
+    onUpdate() {
+        this.router.navigate(["/admin-manage-bike-station"], {
+            state: {
+                bikeStation: this.bikeStation
+            }
+        })
+    }
 
-  performManageRatingsClick() {
-    this.router.navigate(["/bike-station/reviews"], {
-      state: {stationId: this.bikeStation.station_id}
-    });
-  }
+    performManageRatingsClick() {
+        this.router.navigate(["/bike-station/reviews"], {
+            state: {stationId: this.bikeStation.station_id}
+        });
+    }
 
 
 }
