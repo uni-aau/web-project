0dde4ed98ab88c1f9e3a7079d3bebfb9a9eb6b4c
daadii <dadi1@speed.at>
2024-02-21 14:25:59 +0100

    implemented search bar

--- frontend/src/app/components/admin-categories-management/admin-categories-management.component.ts
+++ frontend/src/app/components/admin-categories-management/admin-categories-management.component.ts
@@ -1,43 +1,59 @@
-import { Component, Input } from '@angular/core'
+import {Component, Input} from '@angular/core'
 import {CategoryService} from "../../services/category.service";
 import {PopupService} from "../../services/popup.service";
 
 @Component({
-  selector: 'admin-categories-management',
-  templateUrl: 'admin-categories-management.component.html',
-  styleUrls: ['admin-categories-management.component.css'],
+    selector: 'admin-categories-management',
+    templateUrl: 'admin-categories-management.component.html',
+    styleUrls: ['admin-categories-management.component.css'],
 })
 export class AdminCategoriesManagement {
-  @Input()
-  adminManageCategoriesTitle: string = 'Categories Management'
-  @Input()
-  rootClassName: string = ''
+    @Input()
+    adminManageCategoriesTitle: string = 'Categories Management'
+    @Input()
+    rootClassName: string = ''
 
-  categories: any[] = [];
-  filteredCategories: any[] = [];
+    categories: any[] = [];
+    filteredCategories: any[] = [];
 
-  constructor(private categoryService: CategoryService, private popupService: PopupService) {
-    this.categories = [];
-    this.fetchCategories();
-  }
+    constructor(private categoryService: CategoryService, private popupService: PopupService) {
+        this.categories = [];
+        this.fetchCategories();
+    }
 
-  fetchCategories() {
+    fetchCategories() {
+        this.categoryService.getCategories().subscribe({
+            next: (res) => {
+                this.categories = res;
+                this.performSearch("");
+            },
+            error: (err) => {
+                if (err.status === 404) this.categories = [];
+                console.log(err.error);
+            }
+        })
+    }
 
-  }
+    performSearch(searchTerm: string) {
+        if (!searchTerm) {
+            this.filteredCategories = this.categories;
+        } else {
+            this.categories = this.categories.filter(category => {
+                category.category_name.toLowerCase().includes(searchTerm.toLowerCase())
+            });
+        }
+    }
 
-  performSearch() {
 
-  }
+    performCreate() {
 
-  performCreate() {
+    }
 
-  }
+    handleCategoryDelete() {
 
-  handleCategoryDelete(){
+    }
 
-  }
-
-  handleUpdatedCategory() {
-    this.fetchCategories();
-  }
+    handleUpdatedCategory() {
+        this.fetchCategories();
+    }
 }
