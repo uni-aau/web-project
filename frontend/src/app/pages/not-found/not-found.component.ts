5756e573dd45c86046ed9f6949a515e290dc94d5
daadii <dadi1@speed.at>
2024-02-22 14:33:48 +0100

    fixed navbar in not found

--- frontend/src/app/pages/not-found/not-found.component.ts
+++ frontend/src/app/pages/not-found/not-found.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core'
 import { Title, Meta } from '@angular/platform-browser'
 import {ComponentsModule} from "../../components/components.module";
+import {NgIf} from "@angular/common";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'not-found',
   standalone: true,
-  imports: [ComponentsModule],
+    imports: [ComponentsModule, NgIf],
   templateUrl: 'not-found.component.html',
   styleUrls: ['not-found.component.css'],
 })
 export class NotFound {
-  constructor(private title: Title, private meta: Meta) {
+  constructor(private title: Title, private meta: Meta, public AuthService: AuthService) {
     this.title.setTitle('NotFound - WebProject')
     this.meta.addTags([
       {
